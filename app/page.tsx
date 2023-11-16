74ddb188c04df49568eb9e680a6822eed64e9e07
KelvinMomanyi <kelvinmata42@gmail.com>
2023-11-16 12:59:26 +0300

    new

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp"
+import Features from "@/components/Features";
+
+export default function Home() {
+  return (
+    <>
+     <Hero/>
+     <Camp/>
+     <Guide/>
+     <Features/>
+     <GetApp/>    
+    </>
+  )
+}
